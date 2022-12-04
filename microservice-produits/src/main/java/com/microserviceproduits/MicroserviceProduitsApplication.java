06abbedeb1f0af948a7fd3d869c8aa31ad624491
jamal-soft <jmlaitsalh@gmail.com>
2022-12-04 18:06:22 +0100

    final commit

---
+++ microservice-produits/src/main/java/com/microserviceproduits/MicroserviceProduitsApplication.java
@@ -0,0 +1,14 @@
+package com.microserviceproduits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class MicroserviceProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceProduitsApplication.class, args);
+	}
+
+}
---
+++ microservice-produits/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (0, 'Bougie fonctionnant au feu', 'bougie qui fonctionne comme une ampoule mais sans électricité !', 'https://ma.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/55/068605/1.jpg?4656', 22.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (1, 'Chaise pour s''assoire', 'Chaise rare avec non pas 1 ni 2 mais 3 pieds', 'https://ma.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/55/068605/1.jpg?4656', 95.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (2, 'Cheval pour nains', 'Ce cheval ne portera certainement pas blanche neige, mais sans problème les nains', 'https://ma.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/55/068605/1.jpg?4656', 360.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (3, 'Coq of steel, le superman des volailles', 'Ne passe pas au four', 'https://ma.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/55/068605/1.jpg?4656', 620.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (4, 'Flacon à frotter avec un génie dedans', 'Vous donne droit à l''équivalent de 3/0 voeux', 'https://ma.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/55/068605/1.jpg?4656', 1200.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (5, 'Horloge quantique', 'Donne l''heure, les minutes et même les secondes. Ne fait pas de café', 'https://ma.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/55/068605/1.jpg?4656', 180.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (6, 'Table d''opération pour Hamsters', 'Pour réaliser vos opérations chirugicales sur votre Hamster!', 'https://ma.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/55/068605/1.jpg?4656', 210.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (7 , 'Vase ayant appartenu a Zeus', 'Risque de choc électrique', 'https://ma.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/55/068605/1.jpg?4656', 730.0);
